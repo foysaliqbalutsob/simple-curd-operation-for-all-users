1bb7bb7c0d60a9ede6aa0c3fa601d1f54582749e
my_first_github_repo <foysaliqbalutsob2202113>
2025-11-05 15:51:45 +0600

    bids section aded

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require( 'cors')
-const app = express()
-const port = process.env.PORT || 3000
-
-
-
-
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
 
 // middlewares
 app.use(cors());
@@ -14,101 +10,125 @@ app.use(express.json());
 
 // SmartDB
 // Dq1xWNGdHyL80mkj
-const uri = "mongodb+srv://SmartDB:Dq1xWNGdHyL80mkj@cluster0.um9bwdr.mongodb.net/?appName=Cluster0";
+const uri =
+  "mongodb+srv://SmartDB:Dq1xWNGdHyL80mkj@cluster0.um9bwdr.mongodb.net/?appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
-
-
-
-
-
-
-
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
     const myDB = client.db("Smart_DB");
-    const  productsCollection  = myDB.collection("products");
-    
+    const productsCollection = myDB.collection("products");
+    const bidCollection = myDB.collection('bid_collection')
 
-    app.post('/products', async(req, res)=>{
+
+    app.post("/products", async (req, res) => {
       const product = req.body;
       console.log("New Product:", product);
-      const result = await  productsCollection.insertOne(product);
+      const result = await productsCollection.insertOne(product);
       res.send(result);
     });
 
+    app.get("/products", async (req, res) => {
 
 
-    app.get('/products', async(req, res) =>{
-      const cursor = productsCollection.find();
+
+      console.log(req.query);
+      const email = req.query.email;
+      const query = {};
+      if(email){
+        query.email= email
+      }
+      // const cursor = productsCollection.find().sort({ price_min: -1 });
+      const cursor = productsCollection.find(query).sort({ price_min: -1 });
+      
+
+
+
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get('/products/:id', async(req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const result = await productsCollection.findOne(query);
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("Deleting Product ID:", id);
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+
+      res.send(result);
+    });
+
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      console.log("updating product id: ", id);
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          name: updatedProduct.name,
+          price: updatedProduct.price,
+          brand: updatedProduct.brand,
+        },
+      };
+      const result = await productsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+// bid operation
+
+
+app.get("/bids", async (req, res) => {
+  const email = req.query.email;
+  const query = {}
+  if(email){
+    query.buyer_email = email;
+
+  }
+
+
+  const cursor = bidCollection.find(query);
+  const result = await cursor.toArray();
   res.send(result);
 });
 
+app.post('/bids', async (req, res)=>{
+  const newBid = req.body;
+  const result = await bidCollection.insertOne(newBid);
+  res.send(result)
+});
 
-
-  app.delete('/products/:id', async(req, res)=>{
+// DELETE a bid by ID
+app.delete("/bids/:id", async (req, res) => {
   const id = req.params.id;
-  console.log("Deleting Product ID:", id);
   const query = { _id: new ObjectId(id) };
-  const result = await productsCollection.deleteOne(query);
-
-
+  const result = await bidCollection.deleteOne(query);
   res.send(result);
-})
+});
 
-
- app.patch('/products/:id', async(req, res) =>{
-    const id = req.params.id;
-    const updatedProduct = req.body;
-    console.log('updating product id: ', id);
-    const query = { _id: new ObjectId(id)};
-    const update = {
-      $set: {
-        name: updatedProduct.name,
-        price: updatedProduct.price,
-        brand: updatedProduct.brand,
-
-      }
-    }
-    const result = await productsCollection.updateOne(query, update);
-    res.send(result);
-
-
-  })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.patch("/bids/:id", async (req, res) => {
+  const id = req.params.id;
+  const updatedBid = req.body; // { bid_price: 1200, status: "confirmed" } etc.
+  const query = { _id: new ObjectId(id) };
+  const update = { $set: updatedBid };
+  const result = await bidCollection.updateOne(query, update);
+  res.send(result);
+});
 
 
 
@@ -120,7 +140,9 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -128,10 +150,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello smart deal !!!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello smart deal !!!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
